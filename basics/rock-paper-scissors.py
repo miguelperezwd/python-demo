6673c27eab8de8fc1a9bcaa22079ed1a3bd55278
Miguel Perez <ingmiguelperezr@gmail.com>
2021-03-18 16:01:40 -0400

    New program for simulating rock, paper and scissors game

---
+++ basics/rock-paper-scissors.py
@@ -0,0 +1,17 @@
+# This program simulates the classic Rock-Paper-Scissors game
+import random
+
+computer_choice = random.choice(['scissors','rock','paper'])
+
+user_choice = input("Do you want - rock, paper, or scissors?\n")
+
+if computer_choice == user_choice:
+    print("TIE!")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("YOU WIN!")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("YOU WIN!")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("YOU WIN!")
+else:
+    print("YOU LOOSE :( COMPUTER WINS :D")
