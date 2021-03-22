68f06beb07572595323445cf57b5180d7af41ca8
Miguel Perez <ingmiguelperezr@gmail.com>
2021-03-22 10:57:33 -0400

    Upgraded the loan calculator for showing monthly payments

--- basics/loan-calculator.py
+++ basics/loan-calculator.py
@@ -9,13 +9,19 @@ months = int(input("How many months do you want to see results for?\n"))
 # Determine monthly rate
 monthly_rate = apr/100/12
 
-# Add interest
-interest_paid = money_owed * monthly_rate
-money_owed = money_owed + interest_paid
+for i in range(months):
+    # Add interest
+    interest_paid = money_owed * monthly_rate
+    money_owed = money_owed + interest_paid
 
-# Make payment
-money_owed = money_owed - payment
+    if(money_owed - payment < 0):
+        print("The last payment is", round(money_owed, 2))
+        print("You paid off the loan in", i+1, "months")
+        break
 
-# Show results
-print("Paid", payment, "of which", interest_paid, "was interest", end=' ')
-print("Now I owe", money_owed)
+    # Make payment
+    money_owed = money_owed - payment
+
+    # Show results
+    print("Paid", round(payment, 2), "of which", round(interest_paid, 2), "was interest.", end=' ')
+    print("Now I owe", round(money_owed, 2))
